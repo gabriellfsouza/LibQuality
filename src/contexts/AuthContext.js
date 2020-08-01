23abbfc5816599dde96700612bcec5cff5beddbc
Gabriel Lima <gabriellfsouza@gmail.com>
2020-08-01 17:48:20 -0300

    authorization and registration logic

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -4,39 +4,63 @@ import React, {
 } from 'react';
 import PropTypes from 'prop-types';
 
-import { Auth, Hub } from 'aws-amplify';
+import Amplify, { Auth, Hub } from 'aws-amplify';
 import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
+import awsconfig from '../aws-exports';
+
+Amplify.configure(awsconfig);
 
 const AuthContext = createContext({});
 
-/** @typedef {{
+/**
+ * @typedef {{
  * user: Object
  * }} IAuthData
  */
 
+/**
+ * @typedef {{
+ * payload: {
+ *  event: ('signOut'|'signIn'|'signUp')
+ * }
+ * }} ICapsule
+ */
+
 export function AuthProvider({ children }) {
   const [user, setUser] = useState();
-  const onHubCapsule = useCallback(async (capsule) => {
-    console.log(capsule);
-    debugger;
+
+  const handleVerifyUser = useCallback(async () => {
+    try {
+      const userData = await Auth.currentAuthenticatedUser();
+      if (userData) { setUser(userData); }
+    } catch (error) {
+      console.log('auth error', error);
+    }
   }, []);
+
+  const onHubCapsule = useCallback(
+    /**
+     * @param capsule {ICapsule}
+     */
+    async (capsule) => {
+      if (capsule.payload.event === 'signOut') setUser(undefined);
+      if (capsule.payload.event === 'signIn') handleVerifyUser();
+    }, [],
+  );
 
   useEffect(() => {
     (async () => {
-      try {
-        const userData = await Auth.currentAuthenticatedUser();
-        if (user) { setUser(userData); }
-      } catch (error) {
-        debugger;
-        console.log('auth error', error);
-      }
+      await handleVerifyUser();
       Hub.listen('auth', { onHubCapsule });
     })();
   }, [onHubCapsule]);
 
   return (
     <AuthContext.Provider value={{ user }}>
-      {user ? children : <Authenticator theme={AmplifyTheme} />}
+      <>
+        <Authenticator theme={AmplifyTheme} />
+        {user && children}
+      </>
     </AuthContext.Provider>
   );
 }
